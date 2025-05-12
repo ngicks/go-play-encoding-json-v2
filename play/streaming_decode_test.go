17786d7a0533d5f802b47f1dc49e8153038a5d70
ngicks <yknt.bsl@gmail.com>
2025-05-13 02:42:29 +0900

    add

--- play/streaming_decode_test.go
+++ play/streaming_decode_test.go
@@ -9,8 +9,7 @@ import (
 	"testing"
 )
 
-func TestStreamingDecode(t *testing.T) {
-	const input = `{
+const streamDecodeInput = `{
     "foo": null,
     "bar": {
             "baz": [
@@ -22,7 +21,8 @@ func TestStreamingDecode(t *testing.T) {
 }
 `
 
-	dec := jsontext.NewDecoder(bytes.NewReader([]byte(input)))
+func TestStreamingDecode(t *testing.T) {
+	dec := jsontext.NewDecoder(bytes.NewReader([]byte(streamDecodeInput)))
 	for dec.StackPointer() != jsontext.Pointer("/bar/baz") {
 		_, err := dec.ReadToken()
 		if err != nil {
@@ -36,6 +36,7 @@ func TestStreamingDecode(t *testing.T) {
 	if dec.PeekKind() != '[' {
 		panic("not array")
 	}
+	// discard '['
 	_, err := dec.ReadToken()
 	if err != nil {
 		panic(err)
@@ -58,5 +59,57 @@ func TestStreamingDecode(t *testing.T) {
 	} else {
 		t.Logf("decoded = %#v", decoded)
 		// streaming_decode_test.go:59: decoded = []play.sample{play.sample{Foo:"foo1"}, play.sample{Foo:"foo2"}, play.sample{Foo:"foo3"}}
+	}
+}
+
+func TestStreamingDecode2(t *testing.T) {
+	type Data struct {
+		Foo string `json:"foo"`
+	}
+
+	type Bar struct {
+		Baz []Data `json:"baz"`
+	}
+
+	type sample struct {
+		Foo *int `json:"foo"`
+		Bar Bar  `json:"bar"`
+	}
+
+	dataChan := make(chan Data)
+	unmarshaler := json.WithUnmarshalers(json.UnmarshalFromFunc(func(dec *jsontext.Decoder, d *Data) error {
+		type plain Data
+		var p plain
+		err := json.UnmarshalDecode(dec, &p)
+		if err == nil {
+			dataChan <- Data(p)
+		}
+		*d = Data(p)
+		return err
+	}))
+
+	resultCh := make(chan []Data)
+	go func() {
+		var result []Data
+		for d := range dataChan {
+			result = append(result, d)
+		}
+		resultCh <- result
+	}()
+
+	var s sample
+	err := json.Unmarshal([]byte(streamDecodeInput), &s, unmarshaler)
+	if err != nil {
+		panic(err)
+	}
+	close(dataChan)
+	result := <-resultCh
+
+	expected := []Data{{"foo1"}, {"foo2"}, {"foo3"}}
+	if !reflect.DeepEqual(expected, result) {
+		t.Errorf("not equal:\nexpected(%#v)\n!=\nactual(%#v)", expected, result)
+	} else {
+		t.Logf("decoded = %#v", result)
+		// streaming_decode_test.go:111: decoded = []play.Data{play.Data{Foo:"foo1"}, play.Data{Foo:"foo2"}, play.Data{Foo:"foo3"}}
 	}
 }
